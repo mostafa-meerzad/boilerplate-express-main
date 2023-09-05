193d247446e698a507e36597a5991aae14adf566
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-05 09:34:13 +0430

    send a file using Express

--- myApp.js
+++ myApp.js
@@ -12,6 +12,16 @@ let app = express();
 
 
 
+// console.log("Hello World")
+
+
+// app.get("/", (req, res) => res.send("Hello Express") )
+
+app.get("/", (req, res) =>{
+    // res.sendFile("./views/index.html", {root:"."}) // setting the root directory for the file which is current directory
+
+    res.sendFile(__dirname + "/views/index.html");
+} )
 
 
 
